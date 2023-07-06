478c73d2ecaa092a097d75d0d9bbf93301514e9d
David Derr <david.derr@stud.hs-hannover.de>
2023-07-06 23:01:05 +0200

    app.js umstrukutriert

--- app.js
+++ app.js
@@ -9,14 +9,14 @@ const inputHandler = new InputHandler(0.0, 0.5, 0.0005, 5.0);
 const skybox = await createSkybox(gl);
 skybox.program = await createShaderProgram(gl, './shaders/skybox/vertex.glsl', './shaders/skybox/fragment.glsl');
 
-const carEnvMap = await createBodyChrome(gl, './assets/body.obj');
-carEnvMap.program = await createShaderProgram(gl, './shaders/envmap/vertex.glsl', "./shaders/envmap/fragment.glsl");
+const bodyChrome = await createBodyChrome(gl, './assets/body.obj');
+bodyChrome.program = await createShaderProgram(gl, './shaders/envmap/vertex.glsl', "./shaders/envmap/fragment.glsl");
 
 const tyres = await createTyres(gl, './assets/tyres.obj');
 tyres.program = await createShaderProgram(gl, './shaders/envmap/vertex.glsl', "./shaders/envmap/fragment.glsl");
 
-const carPaint = await createBodyPaint(gl);
-carPaint.program = await createShaderProgram(gl, './shaders/phong/vertex.glsl', "./shaders/phong/fragment.glsl");
+const bodyPaint = await createBodyPaint(gl);
+bodyPaint.program = await createShaderProgram(gl, './shaders/phong/vertex.glsl', "./shaders/phong/fragment.glsl");
 
 // get Locations
 //
@@ -24,8 +24,8 @@ carPaint.program = await createShaderProgram(gl, './shaders/phong/vertex.glsl', 
 var worldMatrix = new Float32Array(16);
 var viewMatrix = new Float32Array(16);
 const projMatrix = new Float32Array(16);
-
 Code4x4.perspective(projMatrix, 45 * Math.PI / 180, canvas.clientWidth / canvas.clientHeight, 0.1, 1000.0);
+
 gl.enable(gl.BLEND);
 gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
 
@@ -40,8 +40,10 @@ const loop = function () {
 	let matProjUniformLocation = gl.getUniformLocation(skybox.program, 'mProj');
 	let matViewUniformLocation = gl.getUniformLocation(skybox.program, 'mView');
 	let matWorldUniformLocation = gl.getUniformLocation(skybox.program, 'mWorld');
+
 	Code4x4.lookAt(viewMatrix, [0, 0.5, 5], [0, 0, 0], [Math.sin(inputHandler.lerpedValue / 10), 1, 0]);
 	Code4x4.rotate(viewMatrix, viewMatrix, speed / 8, [0, 1, 0]);
+
 	gl.uniformMatrix4fv(matProjUniformLocation, gl.FALSE, projMatrix);
 	gl.uniformMatrix4fv(matViewUniformLocation, gl.FALSE, viewMatrix);
 	gl.uniformMatrix4fv(matWorldUniformLocation, gl.FALSE, worldMatrix);
@@ -52,17 +54,16 @@ const loop = function () {
 	//
 	gl.enable(gl.BLEND);
 	gl.enable(gl.DEPTH_TEST);
-	gl.useProgram(carEnvMap.program);
+	gl.useProgram(bodyChrome.program);
 
-	matProjUniformLocation = gl.getUniformLocation(carEnvMap.program, 'mProj');
-	matViewUniformLocation = gl.getUniformLocation(carEnvMap.program, 'mView');
-	matWorldUniformLocation = gl.getUniformLocation(carEnvMap.program, 'mWorld');
-	let eyeDirUniformLocation = gl.getUniformLocation(carEnvMap.program, 'vEyeDir');
-	let useMaskUniformLocation = gl.getUniformLocation(carEnvMap.program, "useMask");
+	matProjUniformLocation = gl.getUniformLocation(bodyChrome.program, 'mProj');
+	matViewUniformLocation = gl.getUniformLocation(bodyChrome.program, 'mView');
+	matWorldUniformLocation = gl.getUniformLocation(bodyChrome.program, 'mWorld');
+	let eyeDirUniformLocation = gl.getUniformLocation(bodyChrome.program, 'vEyeDir');
+	let useMaskUniformLocation = gl.getUniformLocation(bodyChrome.program, "useMask");
+
 	Code4x4.translate(viewMatrix, viewMatrix, [0, 0, 0]);
 	Code4x4.rotate(viewMatrix, viewMatrix, angle / 100, [0.0, 1.0, 0]);
-
-
 	const invViewMatrix = new Float32Array(9);
 	Code3x3.invertFrom4x4(invViewMatrix, viewMatrix);
 	const eyeDir = [0, 0, 1];
@@ -74,8 +75,10 @@ const loop = function () {
 	gl.uniformMatrix4fv(matViewUniformLocation, gl.FALSE, viewMatrix);
 	gl.uniformMatrix4fv(matWorldUniformLocation, gl.FALSE, worldMatrix);
 
-	carEnvMap.draw();
+	bodyChrome.draw();
 
+	// tyres
+	//
 	gl.useProgram(tyres.program);
 
 	matProjUniformLocation = gl.getUniformLocation(tyres.program, 'mProj');
@@ -93,20 +96,20 @@ const loop = function () {
 	tyres.draw();
 	// phong
 	//
-	gl.useProgram(carPaint.program);
+	gl.useProgram(bodyPaint.program);
 
-	matProjUniformLocation = gl.getUniformLocation(carPaint.program, 'mProj');
-	matViewUniformLocation = gl.getUniformLocation(carPaint.program, 'mView');
-	matWorldUniformLocation = gl.getUniformLocation(carPaint.program, 'mWorld');
+	matProjUniformLocation = gl.getUniformLocation(bodyPaint.program, 'mProj');
+	matViewUniformLocation = gl.getUniformLocation(bodyPaint.program, 'mView');
+	matWorldUniformLocation = gl.getUniformLocation(bodyPaint.program, 'mWorld');
 
 	gl.uniformMatrix4fv(matProjUniformLocation, gl.FALSE, projMatrix);
 	gl.uniformMatrix4fv(matViewUniformLocation, gl.FALSE, viewMatrix);
 	gl.uniformMatrix4fv(matWorldUniformLocation, gl.FALSE, worldMatrix);
-
 	gl.uniformMatrix4fv(matViewUniformLocation, gl.FALSE, viewMatrix);
 	gl.uniformMatrix4fv(matWorldUniformLocation, gl.FALSE, worldMatrix);
 
-	carPaint.draw();
+	bodyPaint.draw();
+
 
 	requestAnimationFrame(loop);
 	inputHandler.updateLerpedValue();
